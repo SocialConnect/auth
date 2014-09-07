cda7b639d60e8c825ad0c390394c6e73236557a1
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-07 14:31:21 +1000

    Start write OAuth2\Provider

---
+++ src/Provider/OAuth2/Provider.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ovr
+ * Date: 07.09.14
+ * Time: 14:28
+ */
+
+class Provider
+{
+    /**
+     * @var \SocialConnect\Auth\Service
+     */
+    public $service;
+
+    public function begin()
+    {
+
+    }
+
+    public function finish()
+    {
+
+    }
+
+    public function requestAccessToken()
+    {
+
+    }
+
+    public function getClient()
+    {
+
+    }
+
+    public function getUserIdentity()
+    {
+
+    }
+} 

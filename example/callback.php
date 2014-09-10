a939d761cc98c868d5cdeb51b0f86b37c0b1f047
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-10 16:50:06 +1000

    Strtolower for provider in callback

--- example/callback.php
+++ example/callback.php
@@ -9,7 +9,7 @@ include_once __DIR__ . '/../vendor/autoload.php';
 $configureProviders = include_once 'config.php';
 $service = new \SocialConnect\Auth\Service($configureProviders, null);
 
-$provider = $_GET['provider'];
+$provider = strtolower($_GET['provider']);
 
 switch ($provider) {
     case 'github':

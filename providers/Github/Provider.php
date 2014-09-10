5894e178795ccab7be990da7d7f0b83d14373132
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-10 15:50:51 +1000

    Added simple Github Provider

---
+++ providers/Github/Provider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Vk;
+
+class Github extends \SocialConnect\Auth\Provider\OAuth2\Provider
+{
+    public function getBaseUri()
+    {
+        return 'https://api.github.com/';
+    }
+
+    public function getAuthorizeUri()
+    {
+        return 'https://github.com/login/oauth/authorize';
+    }
+
+    public function getRequestTokenUri()
+    {
+        return 'https://github.com/login/oauth/access_token';
+    }
+}

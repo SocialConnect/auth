1b14c5adc2960e0b198b0590547c665f340a4b53
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-10 16:34:29 +1000

    Added callback url

---
+++ example/callback.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+$configureProviders = include_once 'config.php';
+$service = new \SocialConnect\Auth\Service($configureProviders, null);
+
+$provider = $_GET['provider'];
+
+switch ($provider) {
+    case 'github':
+    case 'vk':
+        $provider = $service->getProvider($provider);
+        break;
+    default:
+        throw new \Exception('Wrong $provider passed in url : ', $provider);
+        break;
+}
+
+$code = $_GET['code'];
+
+var_dump(array(
+    'code' => $code
+));

47c86f99d213943db1e84f0514fab8e11e738f0d
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-14 23:18:59 +1000

    Example added html (show all providers) and redirect after post request

--- example/index.php
+++ example/index.php
@@ -8,9 +8,30 @@ include_once __DIR__ . '/../vendor/autoload.php';
 
 $configureProviders = include_once 'config.php';
 
-$service = new \SocialConnect\Auth\Service($configureProviders, null);
-$service->setHttpClient(new \SocialConnect\Common\Http\Client\Guzzle());
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $service = new \SocialConnect\Auth\Service($configureProviders, null);
+    $service->setHttpClient(new \SocialConnect\Common\Http\Client\Guzzle());
 
-$provider = $service->getProvider('Github');
+    $provider = $service->getProvider('Github');
 
-header('Location: ' . $provider->makeAuthUrl());
+    header('Location: ' . $provider->makeAuthUrl());
+    exit;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SocialConnect | Auth Example</title>
+</head>
+<body>
+    <form action="/" method="post">
+        <?php foreach ($configureProviders['provider'] as $name => $parameters) : ?>
+            <button name="provider" type="submit" value="<?php echo $name; ?>">
+                <?php echo $name; ?>
+            </button>
+        <?php endforeach; ?>
+    </form>
+</body>
+</html>

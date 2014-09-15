10cacfbc915f7fe34551f4a1503827ee885669b8
Дмитрий Пацура <zaets28rus@gmail.com>
2014-09-15 16:39:09 +1000

    Implement Facebook\Provider

---
+++ providers/Facebook/Provider.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Facebook;
+
+use SocialConnect\Auth\Provider\OAuth2\AccessToken;
+use SocialConnect\Common\Entity\User;
+use SocialConnect\Common\Hydrator\ObjectMap;
+
+class Provider extends \SocialConnect\Auth\Provider\OAuth2\Provider
+{
+    public function getBaseUri()
+    {
+        return 'https://graph.facebook.com/';
+    }
+
+    public function getAuthorizeUri()
+    {
+        return 'https://www.facebook.com/dialog/oauth';
+    }
+
+    public function getRequestTokenUri()
+    {
+        return 'https://graph.facebook.com/oauth/access_token';
+    }
+
+    public function getName()
+    {
+        return 'facebook';
+    }
+
+    /**
+     * @param AccessToken $accessToken
+     * @return User
+     */
+    public function getUser(AccessToken $accessToken)
+    {
+        $response = $this->service->getHttpClient()->request($this->getBaseUri() . '/me?access_token=' . $accessToken->getToken());
+        $body = $response->getBody();
+        $result = json_decode($body);
+
+        $hydrator = new ObjectMap(array(
+            'id' => 'id',
+            'first_name' => 'firstname',
+            'last_name' => 'lastname',
+            'gender' => 'sex',
+            'link' => 'url',
+            'locale' => 'locale',
+            'name' => 'username',
+            'timezone' => 'timezone',
+            'updated_time' => 'dateModified',
+            'verified' => 'verified'
+        ));
+
+        return $hydrator->hydrate(new User(), $result);
+    }
+}

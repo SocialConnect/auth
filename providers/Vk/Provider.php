4d615041491f40f7f2acfbcd26a309ae805dd0f3
Patsura Dmitry <zaets28rus@gmail.com>
2014-10-09 19:57:27 +1000

    Vk\Provider remove not-needed slash

--- providers/Vk/Provider.php
+++ providers/Vk/Provider.php
@@ -49,7 +49,7 @@ class Provider extends \SocialConnect\Auth\Provider\OAuth2\Provider
      */
     public function getUser(AccessToken $accessToken)
     {
-        $response = $this->service->getHttpClient()->request($this->getBaseUri() . '/method/users.get?v=5.24&access_token=' . $accessToken->getToken());
+        $response = $this->service->getHttpClient()->request($this->getBaseUri() . 'method/users.get?v=5.24&access_token=' . $accessToken->getToken());
         $body = $response->getBody();
         $result = json_decode($body);
 

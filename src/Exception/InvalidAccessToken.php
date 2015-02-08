daa714722ff1f4977942f68e26ee320a0fbbcab2
Dmitry Patsura <talk@dmtry.me>
2015-02-09 00:56:42 +0900

    Added Exception\InvalidAccessToken

---
+++ src/Exception/InvalidAccessToken.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Auth;
+
+class InvalidAccessToken
+    extends \SocialConnect\Common\Exception
+{
+
+}

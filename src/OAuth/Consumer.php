9384db2876789d125a0c7c65012ae11b967b3b92
Dmitry Patsura <talk@dmtry.me>
2015-08-14 00:06:53 +0900

    [Refactoring] OAuth1 component

--- src/OAuth/Consumer.php
+++ src/OAuth/Consumer.php
@@ -8,10 +8,26 @@ namespace SocialConnect\Auth\OAuth;
 
 class Consumer
 {
+    /**
+     * @var string
+     */
     public $key;
 
+    /**
+     * @var string
+     */
     public $secret;
 
+    /**
+     * @var string|null
+     */
+    public $callback_url;
+
+    /**
+     * @param string $key
+     * @param string $secret
+     * @param string|null $callback_url
+     */
     public function __construct($key, $secret, $callback_url = null)
     {
         $this->key = $key;

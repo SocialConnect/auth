0367da4a424a566eea730efc99c605b8a6a43833
Dmitry Patsura <talk@dmtry.me>
2015-08-16 16:22:50 +0900

    Setup  for InvalidAccessToken

--- src/Exception/InvalidAccessToken.php
+++ src/Exception/InvalidAccessToken.php
@@ -8,5 +8,8 @@ namespace SocialConnect\Auth\Exception;
 
 class InvalidAccessToken extends \SocialConnect\Common\Exception
 {
-
+    public function __construct($message = 'Invalid access token')
+    {
+        parent::__construct($message);
+    }
 }

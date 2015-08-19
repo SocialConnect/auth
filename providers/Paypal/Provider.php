47bd2b2d6a9cee96b8fa116204cebb9bcace658e
Dmitry Patsura <talk@dmtry.me>
2015-08-19 13:10:14 +0900

    Start working on PayPal provider

---
+++ providers/Paypal/Provider.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Paypal;
+
+use SocialConnect\Auth\Exception\InvalidAccessToken;
+use SocialConnect\Auth\Provider\OpenID\AccessToken;
+use SocialConnect\Common\Entity\User;
+use SocialConnect\Common\Hydrator\ObjectMap;
+
+class Provider extends \SocialConnect\Auth\Provider\OpenID\AbstractProvider
+{
+    /**
+     * @return array
+     */
+    public function getAuthUrlParameters()
+    {
+        $default = parent::getAuthUrlParameters();
+        $default['response_type'] = 'code';
+        $default['nonce'] = time() . rand();
+        $default['state'] = time() . rand();
+        $default['scope'] = $this->getScopeInline();
+
+        return $default;
+    }
+
+    public function getBaseUri()
+    {
+        return 'https://api.paypal.com/';
+    }
+
+    public function getAuthorizeUri()
+    {
+        return 'https://www.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize';
+    }
+
+    public function getRequestTokenUri()
+    {
+        return 'https://api.paypal.com/webapps/auth/protocol/openidconnect/v1/tokenservice';
+    }
+
+    public function getName()
+    {
+        return 'paypal';
+    }
+
+    /**
+     * @param string $body
+     * @return AccessToken
+     * @throws InvalidAccessToken
+     */
+    public function parseToken($body)
+    {
+        $result = json_decode($body);
+
+        if (!isset($result->access_token) || empty($result->access_token)) {
+            throw new InvalidAccessToken;
+        }
+
+        return new AccessToken($result->access_token);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentity(AccessToken $accessToken)
+    {
+        $response = $this->service->getHttpClient()->request(
+            $this->getBaseUri() . 'method/users.get',
+            [
+                'v' => '5.24',
+                'access_token' => $accessToken->getToken()
+            ]
+        );
+
+        $body = $response->getBody();
+        $result = json_decode($body);
+
+        $hydrator = new ObjectMap(array(
+            'id' => 'id',
+            'first_name' => 'firstname',
+            'last_name' => 'lastname',
+            'email' => 'email'
+        ));
+
+        return $hydrator->hydrate(new User(), $result->response[0]);
+    }
+}

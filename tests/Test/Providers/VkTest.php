f87d2d2c86a7523f56248445cc34a5bd7fb031e2
Dmitry Patsura <talk@dmtry.me>
2017-01-20 02:26:42 +0900

    [Tests] Vk\Provider - testGetIdentitySuccess

--- tests/Test/Providers/VkTest.php
+++ tests/Test/Providers/VkTest.php
@@ -9,13 +9,22 @@ namespace Test\Providers;
 use SocialConnect\Auth\Exception\InvalidAccessToken;
 use SocialConnect\Auth\Provider\Consumer;
 use SocialConnect\Auth\Provider\OAuth2\AccessToken;
+use SocialConnect\Common\Http\Client\ClientInterface;
 use Test\TestCase;
 
 class VkTest extends TestCase
 {
-    protected function getProvider()
+    /**
+     * @param ClientInterface|null $httpClient
+     * @return \SocialConnect\Vk\Provider
+     */
+    protected function getProvider(ClientInterface $httpClient = null)
     {
         $service = new \SocialConnect\Auth\Service([]);
+
+        if ($httpClient) {
+            $service->setHttpClient($httpClient);
+        }
 
         return new \SocialConnect\Vk\Provider(
             $service,
@@ -58,5 +67,42 @@ class VkTest extends TestCase
         $accessToken = $this->getProvider()->parseToken(
             'lelelelel'
         );
+    }
+
+    public function testGetIdentitySuccess()
+    {
+        $mockedHttpClient = $this->getMockBuilder(\SocialConnect\Common\Http\Client\Curl::class)
+            ->disableProxyingToOriginalMethods()
+            ->getMock();
+
+        $response = new \SocialConnect\Common\Http\Response(
+            200,
+            json_encode(
+                [
+                    'response' => [
+                        [
+                            'id' => $expectedId = 12321312312312,
+                            'first_name' => $expectedFirstname = 'Dmitry',
+                            'last_name' => $expectedLastname = 'Patsura',
+                        ]
+                    ]
+                ]
+            ),
+            []
+        );
+
+        $mockedHttpClient->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
+
+
+        $result = $this->getProvider($mockedHttpClient)->getIdentity(
+            new AccessToken('unknown')
+        );
+
+        parent::assertInstanceOf(\SocialConnect\Common\Entity\User::class, $result);
+        parent::assertSame($expectedId, $result->id);
+        parent::assertSame($expectedFirstname, $result->firstname);
+        parent::assertSame($expectedLastname, $result->lastname);
     }
 }

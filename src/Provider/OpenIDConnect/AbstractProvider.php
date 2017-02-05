cbb6a8e09045ab809b6ac1e8fba724dc8e1394b2
Dmitry Patsura <talk@dmtry.me>
2017-02-05 13:39:51 +0900

    Initial for OpenIDConnect

---
+++ src/Provider/OpenIDConnect/AbstractProvider.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Auth\Provider\OpenIDConnect;
+
+abstract class AbstractProvider extends \SocialConnect\Auth\Provider\OAuth2\AbstractProvider
+{
+}
---
+++ src/Provider/OpenIDConnect/AccessToken.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace SocialConnect\Auth\Provider\OpenIDConnect;
+
+class AccessToken extends \SocialConnect\Auth\Provider\OAuth2\AccessToken
+{
+}

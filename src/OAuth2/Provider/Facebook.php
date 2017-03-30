34a4bff81a46aa615a4cdf853863bb07a32b2ae1
pavellee <chuvak2008@mail.ru>
2017-03-30 15:41:25 +1000

    [OAuth2] Provider\Facebook - fix BC changes related parser

--- src/OAuth2/Provider/Facebook.php
+++ src/OAuth2/Provider/Facebook.php
@@ -6,7 +6,9 @@
 
 namespace SocialConnect\OAuth2\Provider;
 
+use SocialConnect\OAuth2\AccessToken;
 use SocialConnect\Provider\AccessTokenInterface;
+use SocialConnect\Provider\Exception\InvalidAccessToken;
 use SocialConnect\Provider\Exception\InvalidResponse;
 use SocialConnect\Common\Entity\User;
 use SocialConnect\Common\Http\Client\Client;
@@ -39,6 +41,23 @@ class Facebook extends \SocialConnect\OAuth2\AbstractProvider
     public function getName()
     {
         return 'facebook';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parseToken($body)
+    {
+        if (empty($body)) {
+            throw new InvalidAccessToken('Provider response with empty body');
+        }
+
+        $result = json_decode($body, true);
+        if ($result) {
+            return new AccessToken($result);
+        }
+
+        throw new InvalidAccessToken('Provider response with not valid JSON');
     }
 
     /**

29b4e2be706e36dbe1de55d499a4d1177253e89b
Dmitry Patsura <talk@dmtry.me>
2018-02-11 14:57:10 +0900

    [OAuth2] Provider\GitHub - introduce new option: fetch_emails

--- src/OAuth2/Provider/GitHub.php
+++ src/OAuth2/Provider/GitHub.php
@@ -13,6 +13,20 @@ use SocialConnect\Common\Hydrator\ObjectMap;
 
 class GitHub extends \SocialConnect\OAuth2\AbstractProvider
 {
+    /**
+     * @var array
+     */
+    protected $options = [
+        /**
+         * GitHub store only unverified and public email inside User
+         * It's not possible to fetch user with email in GraphQL (new api)
+         * For now, there is only one way, additional request for it by user/email API entrypoint
+         *
+         * It's disabled by default in SocialConnect 1.x, but you can enable it from configuration :)
+         */
+        'fetch_emails' => false
+    ];
+
     public function getBaseUri()
     {
         return 'https://api.github.com/';
@@ -68,6 +82,69 @@ class GitHub extends \SocialConnect\OAuth2\AbstractProvider
             ]
         );
 
-        return $hydrator->hydrate(new User(), $result);
+        /** @var User $user */
+        $user = $hydrator->hydrate(new User(), $result);
+
+        if ($this->getBoolOption('fetch_emails', false)) {
+            $primaryEmail = $this->getPrimaryEmail($accessToken);
+            if ($primaryEmail) {
+                $user->email = $primaryEmail->email;
+                $user->emailVerified = $primaryEmail->verified;
+            }
+        }
+
+        return $user;
+    }
+
+
+    /**
+     * @param AccessTokenInterface $accessToken
+     * @return object
+     * @throws InvalidResponse
+     */
+    public function getPrimaryEmail(AccessTokenInterface $accessToken)
+    {
+        $emails = $this->getEmails($accessToken);
+        if ($emails) {
+            foreach ($emails as $email) {
+                if ($email->primary) {
+                    return $email;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param AccessTokenInterface $accessToken
+     * @return array
+     * @throws InvalidResponse
+     */
+    public function getEmails(AccessTokenInterface $accessToken)
+    {
+        $response = $this->httpClient->request(
+            $this->getBaseUri() . 'user/emails',
+            [
+                'access_token' => $accessToken->getToken()
+            ]
+        );
+
+        if (!$response->isSuccess()) {
+            throw new InvalidResponse(
+                'API response with error code',
+                $response
+            );
+        }
+
+        $result = $response->json();
+        if (!$result) {
+            throw new InvalidResponse(
+                'API response is not a valid JSON object',
+                $response
+            );
+        }
+
+        return $result;
     }
 }

536e7227cfabd58364f2b310c497f9557be77e51
Andreas Heigl <andreas@heigl.org>
2018-08-17 18:36:15 +0200

    Add more tests

--- tests/Test/OAuth2/Provider/MeetupTest.php
+++ tests/Test/OAuth2/Provider/MeetupTest.php
@@ -6,6 +6,12 @@
 
 namespace Test\OAuth2\Provider;
 
+use SocialConnect\Common\Http\Client\ClientInterface;
+use SocialConnect\OAuth2\AccessToken;
+use SocialConnect\OAuth2\Provider\Meetup;
+use SocialConnect\Provider\Consumer;
+use SocialConnect\Provider\Session\SessionInterface;
+
 class MeetupTest extends AbstractProviderTestCase
 {
     /**
@@ -14,5 +20,47 @@ class MeetupTest extends AbstractProviderTestCase
     protected function getProviderClassName()
     {
         return \SocialConnect\OAuth2\Provider\Meetup::class;
+    }
+
+    /** @covers \SocialConnect\OAuth2\Provider\Meetup::parseToken */
+    public function testParsingToken()
+    {
+        $expires = time() + 200;
+        $body = json_encode([
+            'access_token' => 'foo',
+            'expires_in'   => 200,
+            'user_id'      => 'bar',
+            'expires'      => $expires,
+        ]);
+
+        $meetup = new Meetup(
+            self::createMock(ClientInterface::class),
+            self::createMock(SessionInterface::class),
+            self::createMock(Consumer::class),
+            []
+        );
+
+        $token = $meetup->parseToken($body);
+
+        self::assertInstanceOf(AccessToken::class, $token);
+        self::assertAttributeEquals('foo', 'token', $token);
+        self::assertAttributeEquals($expires, 'expires', $token);
+        self::assertAttributeEquals('bar', 'uid', $token);
+    }
+
+    /**
+     * @expectedException \SocialConnect\Provider\Exception\InvalidAccessToken
+     * @covers \SocialConnect\OAuth2\Provider\Meetup::parseToken
+     */
+    public function testParsingTokenFailsWithInvalidBody()
+    {
+        $meetup = new Meetup(
+            self::createMock(ClientInterface::class),
+            self::createMock(SessionInterface::class),
+            self::createMock(Consumer::class),
+            []
+        );
+
+        $meetup->parseToken(json_encode(false));
     }
 }

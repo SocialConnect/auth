5032fe699407c1ef906675cb636b1751df175678
Dmitry Patsura <talk@dmtry.me>
2019-07-02 16:12:50 +0300

    [OAuth2] Feature: Provider\GitHub - rework on top of GraphQL

--- src/OAuth2/Provider/GitHub.php
+++ src/OAuth2/Provider/GitHub.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace SocialConnect\OAuth2\Provider;
 
+use SocialConnect\Common\Http\Client\Client;
 use SocialConnect\Provider\AccessTokenInterface;
 use SocialConnect\Provider\Exception\InvalidResponse;
 use SocialConnect\Common\Entity\User;
@@ -15,20 +16,6 @@ use SocialConnect\Common\Hydrator\ObjectMap;
 class GitHub extends \SocialConnect\OAuth2\AbstractProvider
 {
     const NAME = 'github';
-
-    /**
-     * @var array
-     */
-    protected $options = [
-        /**
-         * GitHub store only unverified and public email inside User
-         * It's not possible to fetch user with email in GraphQL (new api)
-         * For now, there is only one way, additional request for it by user/email API entrypoint
-         *
-         * It's disabled by default in SocialConnect 1.x, but you can enable it from configuration :)
-         */
-        'fetch_emails' => false
-    ];
 
     public function getBaseUri()
     {
@@ -56,10 +43,18 @@ class GitHub extends \SocialConnect\OAuth2\AbstractProvider
     public function getIdentity(AccessTokenInterface $accessToken)
     {
         $response = $this->httpClient->request(
-            $this->getBaseUri() . 'user',
+            $this->getBaseUri() . 'graphql',
             [
-                'access_token' => $accessToken->getToken()
-            ]
+                'body' => json_encode([
+                    'query' => 'query { viewer { databaseId, login, name, email, avatarUrl } }'
+                ])
+            ],
+            [
+                'Authorization' => "bearer {$accessToken->getToken()}",
+                'Accept' => 'application/vnd.github.v4.idl',
+                'Content-Type' => 'application/json'
+            ],
+            Client::POST
         );
 
         if (!$response->isSuccess()) {
@@ -79,77 +74,17 @@ class GitHub extends \SocialConnect\OAuth2\AbstractProvider
 
         $hydrator = new ObjectMap(
             [
-                'id' => 'id',
+                'databaseId' => 'id',
                 'login' => 'username',
                 'email' => 'email',
-                'avatar_url' => 'pictureURL',
+                'avatarUrl' => 'pictureURL',
                 'name' => 'fullname'
             ]
         );
 
         /** @var User $user */
-        $user = $hydrator->hydrate(new User(), $result);
-
-        if ($this->getBoolOption('fetch_emails', false)) {
-            $primaryEmail = $this->getPrimaryEmail($accessToken);
-            if ($primaryEmail) {
-                $user->email = $primaryEmail->email;
-                $user->emailVerified = $primaryEmail->verified;
-            }
-        }
+        $user = $hydrator->hydrate(new User(), $result->data->viewer);
 
         return $user;
-    }
-
-
-    /**
-     * @param AccessTokenInterface $accessToken
-     * @return object
-     * @throws InvalidResponse
-     */
-    public function getPrimaryEmail(AccessTokenInterface $accessToken)
-    {
-        $emails = $this->getEmails($accessToken);
-        if ($emails) {
-            foreach ($emails as $email) {
-                if ($email->primary) {
-                    return $email;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @param AccessTokenInterface $accessToken
-     * @return array
-     * @throws InvalidResponse
-     */
-    public function getEmails(AccessTokenInterface $accessToken)
-    {
-        $response = $this->httpClient->request(
-            $this->getBaseUri() . 'user/emails',
-            [
-                'access_token' => $accessToken->getToken()
-            ]
-        );
-
-        if (!$response->isSuccess()) {
-            throw new InvalidResponse(
-                'API response with error code',
-                $response
-            );
-        }
-
-        $result = $response->json();
-        if (!$result) {
-            throw new InvalidResponse(
-                'API response is not a valid JSON object',
-                $response
-            );
-        }
-
-        return $result;
     }
 }

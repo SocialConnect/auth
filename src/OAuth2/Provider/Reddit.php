e28b8cd3f7c1228f2e21427032eedc320b6a15db
Dmitry Patsura <talk@dmtry.me>
2019-07-03 20:07:34 +0300

    [OAuth2] Feature: Provider\SmashCast - refactoring

--- src/OAuth2/Provider/Reddit.php
+++ src/OAuth2/Provider/Reddit.php
@@ -8,12 +8,9 @@ declare(strict_types=1);
 namespace SocialConnect\OAuth2\Provider;
 
 use Psr\Http\Message\RequestInterface;
-use SocialConnect\Common\Exception\Unsupported;
-use SocialConnect\Common\Http\Client\Client;
 use SocialConnect\OAuth2\AccessToken;
 use SocialConnect\Provider\AccessTokenInterface;
 use SocialConnect\Provider\Exception\InvalidAccessToken;
-use SocialConnect\Provider\Exception\InvalidResponse;
 use SocialConnect\Common\Entity\User;
 use SocialConnect\Common\Hydrator\ObjectMap;
 

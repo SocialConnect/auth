96971532f8572659e6ffecedff569e3f4a7bb137
Dmitry Patsura <talk@dmtry.me>
2019-07-04 17:04:43 +0300

    [Tests] Initial tests for OpenIDConnect/Provider

---
+++ tests/Test/OpenIDConnect/Provider/PixelPinTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Test\OpenIDConnect\Provider;
+
+class PixelPinTest extends AbstractProviderTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getProviderClassName()
+    {
+        return \SocialConnect\OpenIDConnect\Provider\Google::class;
+    }
+}

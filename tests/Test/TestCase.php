ee2f44da7d8eb92d07bcb8180733c45a2d72bcfd
Dmitry Patsura <talk@dmtry.me>
2019-07-04 13:09:03 +0300

    [Tests] Feature: Reduce copy/paste

--- tests/Test/TestCase.php
+++ tests/Test/TestCase.php
@@ -7,10 +7,54 @@
 namespace Test;
 
 use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use ReflectionClass;
 use SocialConnect\Auth\Service;
+use function GuzzleHttp\Psr7\stream_for;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
+
+    /**
+     * @param string|null $responseData
+     * @param int $responseCode
+     * @return \PHPUnit_Framework_MockObject_MockObject|ResponseInterface
+     */
+    protected function mockClientResponse($responseData, $responseCode = 200)
+    {
+        $mockedHttpClient = $this->getMockBuilder(ClientInterface::class)
+            ->getMock();
+
+        $response = new \GuzzleHttp\Psr7\Response(
+            $responseCode,
+            [],
+            stream_for($responseData)
+        );
+
+        $mockedHttpClient->expects($this->once())
+            ->method('sendRequest')
+            ->willReturn($response);
+
+        return $mockedHttpClient;
+    }
+
+    /**
+     * @param object $object
+     * @param string $name
+     * @param array $params
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    protected static function callProtectedMethod($object, $name, array $params = [])
+    {
+        $class = new ReflectionClass($object);
+
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $params);
+    }
+
     protected function getService()
     {
         $httpClient = $this->getMockBuilder(ClientInterface::class)

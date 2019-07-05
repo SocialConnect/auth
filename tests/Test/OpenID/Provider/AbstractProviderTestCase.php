e4a118dd01aa3103f705ffed4d82ca38dbb6b903
Dmitry Patsura <talk@dmtry.me>
2019-07-06 01:04:56 +0300

    [OpenID] Feature: Add more tests

--- tests/Test/OpenID/Provider/AbstractProviderTestCase.php
+++ tests/Test/OpenID/Provider/AbstractProviderTestCase.php
@@ -6,6 +6,10 @@
 
 namespace Test\OpenID\Provider;
 
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use SocialConnect\OAuth2\AccessToken;
+
 abstract class AbstractProviderTestCase extends \Test\Provider\AbstractProviderTestCase
 {
     public function testGetOpenIDUrl()
@@ -21,5 +25,28 @@ abstract class AbstractProviderTestCase extends \Test\Provider\AbstractProviderT
     public function testGetNameReturnString()
     {
         parent::assertInternalType('string', $this->getProvider()->getName());
+    }
+
+    /**
+     * @return ResponseInterface
+     */
+    abstract protected function getTestResponseForGetIdentity(): ResponseInterface;
+
+    public function testGetIdentitySuccess()
+    {
+        $mockedHttpClient = $this->getMockBuilder(ClientInterface::class)
+            ->getMock();
+
+        $mockedHttpClient->expects($this->once())
+            ->method('sendRequest')
+            ->willReturn($this->getTestResponseForGetIdentity());
+
+        $this->getProvider($mockedHttpClient)->getIdentity(
+            new AccessToken(
+                [
+                    'access_token' => '123456789'
+                ]
+            )
+        );
     }
 }

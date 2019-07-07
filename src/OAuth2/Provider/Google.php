cd20b1b17e55f811d29388679afdb2ecab57ddba
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-07-07 22:53:35 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/OAuth2/Provider/Google.php
+++ src/OAuth2/Provider/Google.php
@@ -9,10 +9,7 @@ declare(strict_types=1);
 namespace SocialConnect\OAuth2\Provider;
 
 use SocialConnect\Provider\AccessTokenInterface;
-use SocialConnect\Provider\Exception\InvalidAccessToken;
-use SocialConnect\Provider\Exception\InvalidResponse;
 use SocialConnect\OAuth2\AbstractProvider;
-use SocialConnect\OAuth2\AccessToken;
 use SocialConnect\Common\Entity\User;
 use SocialConnect\Common\Hydrator\ObjectMap;
 

7bb25ceb4d0100cced78db9c99e215e9cc68a1ca
Dmitry Patsura <talk@dmtry.me>
2019-09-15 11:48:48 +0300

    [Provider] Feature: Improve code quality for request method

---
+++ src/Provider/Exception/InvalidRequest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * SocialConnect project
+ * @author: Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+declare(strict_types=1);
+
+namespace SocialConnect\Provider\Exception;
+
+use Throwable;
+
+class InvalidRequest extends \SocialConnect\Common\Exception
+{
+}
